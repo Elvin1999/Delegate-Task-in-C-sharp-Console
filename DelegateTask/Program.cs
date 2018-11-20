cf0b6b7e6b62156e703568c731a622d6d62f2cad
camalzade_elvin <camalzade_elvin@outlook.com>
2018-11-21 01:10:09 +0400

    Myclass is ready

--- DelegateTask/Program.cs
+++ DelegateTask/Program.cs
@@ -5,11 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 namespace DelegateTask
 {
+    class MyClass
+    {
+        public void Space(string str)
+        {
+            var str2 = string.Empty;
+            for (int i = 0; i < str.Length; i++)
+            {
+                str2 += str[i] + " ";
+            }
+            Console.WriteLine(str2);
+        }
+        public void Reverse(string str)
+        {
+            var newstr=str.Reverse();
+            Console.WriteLine(newstr);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            //   Console.WriteLine("Enter string");
 
+            // var str = Console.ReadLine();
+            //       MyClass cls = new MyClass(str);
+            //     Func funcDell = new Func(params) // params sadece sizin ora vereceyiniz parametrlerdi	
+
+            // burda funcDell-e functionlari verirsiniz
+
+            // Run run = new Run();
+            //         run.runFunc(funcDell, str); //cagiranda Space() ve Reverse() functionlari ise dusmelidir
+            //     }
         }
     }
 }
